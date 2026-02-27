412364e84479c1db82fd2c10636cea814bfa2a7c
unknown <noelynsoriano241@gmail.com>
2026-02-27 21:41:43 +0800

    Add nav button

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:studybuddy/screens/create_page.dart';
 import 'package:studybuddy/screens/login_page.dart';
+import 'package:studybuddy/screens/home_page.dart';
+import 'package:studybuddy/screens/profile_page.dart';
 import 'package:studybuddy/screens/register_page.dart';
+import 'package:studybuddy/screens/study_page.dart';
 import 'package:studybuddy/services/firebase_service.dart';
 import 'package:studybuddy/screens/opening_page.dart';
 import 'package:studybuddy/screens/landing_page.dart';
@@ -25,6 +29,10 @@ class MyApp extends StatelessWidget {
        'landing': (context) => const LandingPage(),
        'login': (context) => const LoginPage(),
        'register':(context) => const RegisterPage(),
+       'home':(context) => const HomePage(),
+       'create':(context) => const CreatePage(),
+       'study':(context) => const StudyPage(),
+       'profile':(context) => const ProfilePage(),
       },
     );
   }
