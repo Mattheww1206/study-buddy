e3230c2f40859e1a65eda71d2d546e4f54d4abb4
unknown <noelynsoriano241@gmail.com>
2026-03-04 19:45:36 +0800

    profile update

--- lib/features/profile/presentation/account_information_page.dart
+++ lib/features/profile/presentation/account_information_page.dart
@@ -1,4 +1,4 @@
-import 'dart:io'; 
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,6 +12,7 @@ class AccountInformationPage extends StatefulWidget {
 
 class _AccountInformationPageState extends State<AccountInformationPage> {
   File? _selectedImage;
+  String _username = ''; 
 
   Future<void> _pickImage() async {
     final ImagePicker picker = ImagePicker();
@@ -24,10 +25,82 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
     }
   }
 
+  void _showEditDialog(String title, String currentValue, Function(String) onSave) {
+    TextEditingController controller = TextEditingController(text: currentValue);
+    showDialog(
+      context: context,
+      builder: (context) => Dialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+        child: Container(
+          padding: const EdgeInsets.all(25),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Edit $title',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.lora(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                controller: controller,
+                textAlign: TextAlign.center,
+                style: GoogleFonts.inter(fontSize: 18),
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.grey.shade300)),
+                  focusedBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Color(0xFF1A0B70))),
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                children: [
+                  Expanded(
+                    child: OutlinedButton(
+                      onPressed: () => Navigator.pop(context),
+                      style: OutlinedButton.styleFrom(
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        side: const BorderSide(color: Color(0xFF1A0B70)),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                      ),
+                      child: Text('Stay', style: GoogleFonts.inter(color: const Color(0xFF1A0B70), fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                  const SizedBox(width: 15),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        onSave(controller.text);
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF1A0B70),
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+                      ),
+                      child: Text('Save', style: GoogleFonts.inter(color: Colors.white, fontWeight: FontWeight.bold)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF1A0B70), 
+      backgroundColor: const Color(0xFF1A0B70),
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
@@ -39,7 +112,7 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
         title: Text(
           'Account',
           style: GoogleFonts.lora(
-            fontWeight:FontWeight.bold,
+            fontWeight: FontWeight.bold,
             fontSize: 30,
             color: Colors.black,
           ),
@@ -71,11 +144,11 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
                       textAlign: TextAlign.left,
                       style: GoogleFonts.lora(
                         fontWeight: FontWeight.bold,
-                        fontSize: 23, 
-                    ),
+                        fontSize: 23,
+                      ),
                     ),
                   ),
-                
+
                   Padding(
                     padding: const EdgeInsets.all(20.0),
                     child: Row(
@@ -86,24 +159,22 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
                           height: 100,
                           decoration: BoxDecoration(
                             shape: BoxShape.circle,
-                          
-                            image: _selectedImage != null 
-                              ? DecorationImage(
-                                  image: FileImage(_selectedImage!),
-                                  fit: BoxFit.cover,
-                                ) 
-                              : null,
+                            image: _selectedImage != null
+                                ? DecorationImage(
+                                    image: FileImage(_selectedImage!),
+                                    fit: BoxFit.cover,
+                                  )
+                                : null,
                             gradient: const LinearGradient(
                               begin: Alignment.topCenter,
                               colors: [Color(0xFF90CAF9), Color(0xFFE1F5FE)],
                             ),
                           ),
-                         
-                          child: _selectedImage == null 
-                            ? const Icon(Icons.person, size: 90, color: Colors.black54)
-                            : null,
+                          child: _selectedImage == null
+                              ? const Icon(Icons.person, size: 90, color: Colors.black54)
+                              : null,
                         ),
-            
+
                         GestureDetector(
                           onTap: _pickImage,
                           child: Container(
@@ -112,15 +183,15 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
                               color: Colors.orange,
                               borderRadius: BorderRadius.circular(20),
                             ),
-                            child: Row(
-                              children: const [
+                            child: const Row(
+                              children: [
                                 Icon(Icons.camera_alt, color: Colors.white, size: 23),
                                 SizedBox(width: 5),
-                                Text('Edit Photo', 
-                                style: TextStyle(
-                                  color: Colors.white, 
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold)),
+                                Text('Edit Photo',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold)),
                               ],
                             ),
                           ),
@@ -128,69 +199,53 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
                       ],
                     ),
                   ),
-                  // Full Name
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                    decoration: const BoxDecoration(border: Border(top: BorderSide(color: Colors.grey, width: 0.5))),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          'Full Name', 
-                          style: GoogleFonts.inter(fontSize: 20)),
-                        Text(
-                          'Matthew Manarang', 
-                          style: GoogleFonts.lora(fontSize: 20, 
-                          fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                  ),
+
                   // Username
                   Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-                    decoration: const BoxDecoration(
-                      border: Border(
-                        top: BorderSide(
-                          color: Colors.grey, 
-                          width: 0.5))),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 8),
+                    decoration: const BoxDecoration(border: Border(top: BorderSide(color: Colors.grey, width: 0.5))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(
-                          'Username', 
-                          style: GoogleFonts.inter(
-                            fontSize: 20)),
-                        Text(
-                          'Matthew', 
-                          style: GoogleFonts.lora(
-                            fontSize: 20, 
-                            fontWeight: FontWeight.bold)),
+                        Text('Username', style: GoogleFonts.inter(fontSize: 14, color: Colors.grey[600])),
+                        const SizedBox(height: 2),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                _username,
+                                style: GoogleFonts.lora(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            const SizedBox(width: 8),
+                            GestureDetector(
+                              onTap: () => _showEditDialog('Username', _username, (val) => setState(() => _username = val)),
+                              child: const Icon(Icons.edit, size: 18, color: Colors.blue),
+                            ),
+                          ],
+                        ),
                       ],
                     ),
                   ),
-                  // Email
+
+                  // Email 
                   Container(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 20, 
-                      vertical: 15),
-                    decoration: const BoxDecoration
-                    (border: Border(
-                      top: BorderSide(
-                        color: Colors.grey, 
-                        width: 0.5))),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 15),
+                    decoration: const BoxDecoration(border: Border(top: BorderSide(color: Colors.grey, width: 0.5))),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
+                        Text('Email', style: GoogleFonts.inter(fontSize: 14, color: Colors.grey[600])),
+                        const SizedBox(height: 2),
                         Text(
-                          'Email', 
-                          style: GoogleFonts.inter(
-                            fontSize: 20)),
-                        Text(
-                          'matthew@gmail.com', 
+                          'matthew@gmail.com',
                           style: GoogleFonts.lora(
-                            fontSize: 20, 
-                            fontWeight: FontWeight.bold,
-                             decoration: TextDecoration.underline)),
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.underline),
+                        ),
                       ],
                     ),
                   ),
@@ -200,7 +255,7 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
 
             const SizedBox(height: 20),
 
-            // SECURITY SECTION 
+            // SECURITY 
             Container(
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -210,50 +265,29 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
                 children: [
                   Container(
                     width: double.infinity,
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 12, horizontal: 25),
+                    padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 25),
                     decoration: const BoxDecoration(
                       color: Color(0xFFE3F2FD),
                       borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30)),
                     ),
-                    child: Text(
-                      'Security', 
-                      style: GoogleFonts.inter(
-                        fontSize: 20, 
-                        fontWeight: FontWeight.bold)),
+                    child: Text('Security', style: GoogleFonts.inter(fontSize: 20, fontWeight: FontWeight.bold)),
                   ),
-          
                   TextButton(
                     style: TextButton.styleFrom(
                       padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                       foregroundColor: Colors.black,
                       shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(30),
-                          bottomRight: Radius.circular(30),
-                        ),
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30), bottomRight: Radius.circular(30)),
                       ),
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'change_password');
-                    },
+                    onPressed: () => Navigator.pushNamed(context, 'change_password'),
                     child: Row(
                       children: [
-                        const Icon(
-                          Icons.lock_outline, 
-                          color: Colors.blue, 
-                          size: 35),
+                        const Icon(Icons.lock_outline, color: Colors.blue, size: 35),
                         const SizedBox(width: 15),
-                        Text(
-                          'Change Password', 
-                          style: GoogleFonts.lora(
-                            fontSize: 20, 
-                            fontWeight: FontWeight.bold)),
+                        Text('Change Password', style: GoogleFonts.lora(fontSize: 20, fontWeight: FontWeight.bold)),
                         const Spacer(),
-                        const Icon(
-                          Icons.arrow_forward_ios, 
-                          color: Colors.black, 
-                          size: 20),
+                        const Icon(Icons.arrow_forward_ios, color: Colors.black, size: 20),
                       ],
                     ),
                   ),
@@ -263,7 +297,7 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
 
             const SizedBox(height: 20),
 
-            // DELETE ACCOUNT 
+            // DELETE ACCOUNT
             Container(
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -281,28 +315,15 @@ class _AccountInformationPageState extends State<AccountInformationPage> {
                     ),
                     child: Row(
                       children: [
-                        const Icon
-                        (Icons.warning_amber_rounded, 
-                        color: Colors.red, 
-                        size: 28),
+                        const Icon(Icons.warning_amber_rounded, color: Colors.red, size: 28),
                         const SizedBox(width: 10),
-                        Text(
-                          'Delete Account', 
-                          style: GoogleFonts.inter(
-                            fontSize: 20, 
-                            fontWeight: FontWeight.bold, 
-                            color: Colors.red.shade900)),
+                        Text('Delete Account', style: GoogleFonts.inter(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.red.shade900)),
                       ],
                     ),
                   ),
                   Padding(
                     padding: const EdgeInsets.all(20.0),
-                    child: Text(
-                      'This action is permanent and cannot be undone.',
-                      style: GoogleFonts.lora(
-                        fontSize: 20, 
-                        color: Colors.black87),
-                    ),
+                    child: Text('This action is permanent and cannot be undone.', style: GoogleFonts.lora(fontSize: 20, color: Colors.black87)),
                   ),
                 ],
               ),
